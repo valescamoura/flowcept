e7310d176864042c15afb63618091c4b04cdc472
Valesca Moura <valescamoura@id.uff.br>
2026-08-10 18:13:37 -0400

    fix

--- examples/llm_tutorial/llm_train_campaign.py
+++ examples/llm_tutorial/llm_train_campaign.py
@@ -369,10 +369,12 @@ def start_dask(
 
             scheduler_process = run_command(
                 scheduler_command,
-                # out_file="dask_scheduler.out",
-                # err_file="dask_scheduler.err",
-                # env=env,
+                out_file="dask_scheduler.out",
+                err_file="dask_scheduler.err",
+                env=env,
             )
+
+            sleep(5)
 
             timeout = 60
             start = time.time()
